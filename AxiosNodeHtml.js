f88a03529c7bf63d798fa6f3ac4d7b303278e2ab
NatthaphatSuknakkit <6406021620033@fitm.kmutnb.ac.th>
2024-02-12 10:39:55 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,13 +8,13 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://10.104.11.66:3000";
+const base_url = "http://localhost:3000";
 
 // Set the template engine
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.set ("views" , path.join(__dirname, "/public/views"))
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", async (req, res) => {
